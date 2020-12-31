4f356b2d5a7ca98a3a021c350dfecb3edf5e7f13
vadis365 <curve_linear@hotmail.com>
2020-12-31 19:16:47 +0000

    fixy

--- src/main/java/thebetweenlands/client/render/model/entity/ModelPuffin.java
+++ src/main/java/thebetweenlands/client/render/model/entity/ModelPuffin.java
@@ -35,6 +35,8 @@ public class ModelPuffin extends MowzieModelBase {
 	    MowzieModelRenderer wingright1;
 	    MowzieModelRenderer wingright2;
 
+	    MowzieModelRenderer[] parts;
+
 	    public ModelPuffin() {
 	        textureWidth = 64;
 	        textureHeight = 32;
